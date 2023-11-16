bcd0816a3c56ba4d55208c0b27f374422f4a93ae
Hina samreen <66202956+Hina-samreen@users.noreply.github.com>
2023-11-16 20:18:42 +0100

    Add files via upload

---
+++ resources/scenarios/function-jim-links-1698393751681.js
@@ -0,0 +1,19 @@
+(function(window, undefined) {
+
+  var jimLinks = {
+    "c77868ab-3db2-4d1f-9cc4-fea929466636" : {
+    },
+    "d12245cc-1680-458d-89dd-4f0d7fb22724" : {
+      "Button_6" : [
+        "3b20064a-7b54-40c3-be71-ae5ffb079cb1"
+      ]
+    },
+    "3b20064a-7b54-40c3-be71-ae5ffb079cb1" : {
+      "Input_2" : [
+        "c77868ab-3db2-4d1f-9cc4-fea929466636"
+      ]
+    }    
+  }
+
+  window.jimLinks = jimLinks;
+})(window);
