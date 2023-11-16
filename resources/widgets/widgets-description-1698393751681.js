d8501784ae91d9c7f14a416f9c5f51ea15495bd3
Hina samreen <66202956+Hina-samreen@users.noreply.github.com>
2023-11-16 20:24:41 +0100

    Add files via upload

---
+++ resources/widgets/widgets-description-1698393751681.js
@@ -0,0 +1,50 @@
+	window.widgets = {
+		descriptionMap : widgetDescriptionMap = {},
+		rootWidgetMap : widgetRootMap = {}
+	};
+
+	widgets.descriptionMap[["s-Paragraph_68", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Paragraph_68", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Email", "s-Group_19"]; 
+
+	widgets.descriptionMap[["s-Input_10", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Input_10", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Email", "s-Group_19"]; 
+
+	widgets.descriptionMap[["s-Path_93", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Path_93", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Email", "s-Group_19"]; 
+
+	widgets.descriptionMap[["s-Paragraph_67", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Paragraph_67", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Email", "s-Group_19"]; 
+
+	widgets.descriptionMap[["s-Input_11", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Input_11", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Password", "s-Group_20"]; 
+
+	widgets.descriptionMap[["s-Path_94", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Path_94", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Password", "s-Group_20"]; 
+
+	widgets.descriptionMap[["s-Paragraph_72", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Paragraph_72", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Password", "s-Group_20"]; 
+
+	widgets.descriptionMap[["s-Paragraph_70", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Paragraph_70", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Password", "s-Group_20"]; 
+
+	widgets.descriptionMap[["s-Button_6", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Button_6", "d12245cc-1680-458d-89dd-4f0d7fb22724"]] = ["Basic button", "s-Button_6"]; 
+
+	widgets.descriptionMap[["s-Rectangle_7", "3b20064a-7b54-40c3-be71-ae5ffb079cb1"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Rectangle_7", "3b20064a-7b54-40c3-be71-ae5ffb079cb1"]] = ["Align center", "s-Group_6"]; 
+
+	widgets.descriptionMap[["s-Paragraph_11", "3b20064a-7b54-40c3-be71-ae5ffb079cb1"]] = ""; 
+
+			widgets.rootWidgetMap[["s-Paragraph_11", "3b20064a-7b54-40c3-be71-ae5ffb079cb1"]] = ["h3", "s-Paragraph_11"]; 
+
+	
