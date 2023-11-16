ca3e5212371d5185056a212155b429502d09c130
Hina samreen <66202956+Hina-samreen@users.noreply.github.com>
2023-11-16 20:09:54 +0100

    Add files via upload

---
+++ resources/lookup-1698393751681.js
@@ -0,0 +1,32 @@
+(function(window, undefined) {
+  var dictionary = {
+    "c77868ab-3db2-4d1f-9cc4-fea929466636": "Brands_Page",
+    "d12245cc-1680-458d-89dd-4f0d7fb22724": "Screen 1",
+    "3b20064a-7b54-40c3-be71-ae5ffb079cb1": "Dashboard",
+    "f39803f7-df02-4169-93eb-7547fb8c961a": "Template 1",
+    "bb8abf58-f55e-472d-af05-a7d1bb0cc014": "default"
+  };
+
+  var uriRE = /^(\/#)?(screens|templates|masters|scenarios)\/(.*)(\.html)?/;
+  window.lookUpURL = function(fragment) {
+    var matches = uriRE.exec(fragment || "") || [],
+        folder = matches[2] || "",
+        canvas = matches[3] || "",
+        name, url;
+    if(dictionary.hasOwnProperty(canvas)) { /* search by name */
+      url = folder + "/" + canvas;
+    }
+    return url;
+  };
+
+  window.lookUpName = function(fragment) {
+    var matches = uriRE.exec(fragment || "") || [],
+        folder = matches[2] || "",
+        canvas = matches[3] || "",
+        name, canvasName;
+    if(dictionary.hasOwnProperty(canvas)) { /* search by name */
+      canvasName = dictionary[canvas];
+    }
+    return canvasName;
+  };
+})(window);
